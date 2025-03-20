c93af1d2380f5c9e55404d0423522b47bdeae87f
Jordan-Williams2 <Jordan.Williams2@ibm.com>
2025-03-20 19:37:52 +0000

    feat: move logs agents to its own tile SKIP UPGRADE TEST

--- solutions/fully-configurable/variables.tf
+++ solutions/fully-configurable/variables.tf
@@ -1,10 +1,16 @@
+variable "ibmcloud_api_key" {
+  type        = string
+  description = "The IBM Cloud API key."
+  sensitive   = true
+}
+
 ##############################################################################
 # Cluster variables
 ##############################################################################
 
 variable "cluster_id" {
   type        = string
-  description = "The ID of the cluster to deploy the agent."
+  description = "The ID of the cluster to deploy the agent in."
 }
 
 variable "cluster_resource_group_id" {
@@ -13,36 +19,22 @@ variable "cluster_resource_group_id" {
 }
 
 variable "cluster_config_endpoint_type" {
-  description = "The type of endpoint to use for the cluster config access: `default`, `private`, `vpe`, or `link`. The `default` value uses the default endpoint of the cluster."
+  description = "Specify the type of endpoint to use to access the cluster configuration. Possible values: `default`, `private`, `vpe`, `link`. The `default` value uses the default endpoint of the cluster."
   type        = string
-  default     = "default"
+  default     = "private"
   nullable    = false # use default if null is passed in
-  validation {
-    error_message = "Invalid endpoint type. Valid values are `default`, `private`, `vpe`, or `link`."
-    condition     = contains(["default", "private", "vpe", "link"], var.cluster_config_endpoint_type)
-  }
 }
 
 variable "is_vpc_cluster" {
-  description = "Specify true if the target cluster for the agent is a VPC cluster, false if it is a classic cluster."
   type        = bool
+  description = "Specify true if the target cluster for the DA is a VPC cluster, false if it is classic cluster."
   default     = true
 }
 
 variable "wait_till" {
-  description = "To avoid long wait times when you run your Terraform code, you can specify the stage when you want Terraform to mark the cluster resource creation as completed. Depending on what stage you choose, the cluster creation might not be fully completed and continues to run in the background. However, your Terraform code can continue to run without waiting for the cluster to be fully created. Supported args are `MasterNodeReady`, `OneWorkerNodeReady`, `IngressReady` and `Normal`"
+  description = "Specify the stage when Terraform should mark the cluster resource creation as completed. Supported values: `MasterNodeReady`, `OneWorkerNodeReady`, `IngressReady`, `Normal`."
   type        = string
   default     = "Normal"
-
-  validation {
-    error_message = "`wait_till` value must be one of `MasterNodeReady`, `OneWorkerNodeReady`, `IngressReady` or `Normal`."
-    condition = contains([
-      "MasterNodeReady",
-      "OneWorkerNodeReady",
-      "IngressReady",
-      "Normal"
-    ], var.wait_till)
-  }
 }
 
 variable "wait_till_timeout" {
@@ -69,30 +61,21 @@ variable "logs_agent_namespace" {
   nullable    = false
 }
 
-variable "logs_agent_trusted_profile" {
+variable "logs_agent_trusted_profile_id" {
   type        = string
-  description = "The IBM Cloud trusted profile ID. Used only when `logs_agent_iam_mode` is set to `TrustedProfile`. The trusted profile must have an IBM Cloud Logs `Sender` role."
+  description = "The IBM Cloud trusted profile ID. Used only when `logs_agent_iam_mode` is set to `TrustedProfile`. The trusted profile must have an IBM Cloud Logs `Sender` role. Must provide a value for `logs_agent_iam_api_key` if `logs_agent_trusted_profile_id` is null."
   default     = null
-  validation {
-    condition     = !(var.logs_agent_trusted_profile == null && var.logs_agent_iam_mode == "TrustedProfile")
-    error_message = "The `logs_agent_trusted_profile` is required when `logs_agent_iam_mode` is set to `TrustedProfile`."
-  }
 }
-
 
 variable "logs_agent_iam_api_key" {
   type        = string
   description = "The IBM Cloud API key for the Logs agent to authenticate and communicate with the IBM Cloud Logs. It is required if `logs_agent_iam_mode` is set to `IAMAPIKey`."
   sensitive   = true
   default     = null
-  validation {
-    condition     = !(var.logs_agent_iam_mode == "IAMAPIKey" && var.logs_agent_iam_api_key == null)
-    error_message = "The `logs_agent_iam_api_key` is required when `logs_agent_iam_mode` is set to `IAMAPIKey`."
-  }
 }
 
 variable "logs_agent_tolerations" {
-  description = "List of tolerations to apply to Logs agent. The default value means a pod will run on every node."
+  description = "List of tolerations to apply to Logs agent."
   type = list(object({
     key               = optional(string)
     operator          = optional(string)
@@ -107,7 +90,7 @@ variable "logs_agent_tolerations" {
 
 variable "logs_agent_additional_log_source_paths" {
   type        = list(string)
-  description = "The list of additional log sources. By default, the Logs agent collects logs from a single source at `/var/log/containers/*.log`."
+  description = "The list of additional log sources. By default, the Logs agent collects logs from a single source at `/var/log/containers/logger-agent-ds-*.log`."
   default     = []
   nullable    = false
 }
@@ -121,14 +104,14 @@ variable "logs_agent_exclude_log_source_paths" {
 
 variable "logs_agent_selected_log_source_paths" {
   type        = list(string)
-  description = "The list of specific log sources paths. Logs will only be collected from the specified log source paths. If no paths are specified, it will send logs from `/var/log/containers`."
+  description = "The list of specific log sources paths. Logs will only be collected from the specified log source paths."
   default     = []
   nullable    = false
 }
 
 variable "logs_agent_log_source_namespaces" {
   type        = list(string)
-  description = "The list of namespaces from which logs should be forwarded by agent. If namespaces are not listed, logs from all namespaces will be sent."
+  description = "The list of namespaces from which logs should be forwarded by agent. When specified logs from only these namespaces will be sent by the agent."
   default     = []
   nullable    = false
 }
@@ -137,20 +120,12 @@ variable "logs_agent_iam_mode" {
   type        = string
   default     = "TrustedProfile"
   description = "IAM authentication mode: `TrustedProfile` or `IAMAPIKey`."
-  validation {
-    error_message = "The IAM mode can only be `TrustedProfile` or `IAMAPIKey`."
-    condition     = contains(["TrustedProfile", "IAMAPIKey"], var.logs_agent_iam_mode)
-  }
 }
 
 variable "logs_agent_iam_environment" {
   type        = string
   default     = "PrivateProduction"
-  description = "IAM authentication Environment: `Production` or `PrivateProduction` or `Staging` or `PrivateStaging`. `Production` specifies the public endpoint & `PrivateProduction` specifies the private endpoint."
-  validation {
-    error_message = "The IAM environment can only be `Production` or `PrivateProduction` or `Staging` or `PrivateStaging`."
-    condition     = contains(["Production", "PrivateProduction", "Staging", "PrivateStaging"], var.logs_agent_iam_environment)
-  }
+  description = "IAM authentication Environment: `Production` or `PrivateProduction` or `Staging` or `PrivateStaging`."
 }
 
 variable "logs_agent_additional_metadata" {
@@ -162,7 +137,7 @@ variable "logs_agent_additional_metadata" {
   default = []
 }
 
-variable "logs_agent_enable_scc" {
+variable "is_ocp_cluster" {
   description = "Whether to enable creation of Security Context Constraints in Openshift. When installing on an OpenShift cluster, this setting is mandatory to configure permissions for pods within your cluster."
   type        = bool
   default     = true
@@ -171,14 +146,11 @@ variable "logs_agent_enable_scc" {
 variable "cloud_logs_ingress_endpoint" {
   description = "The host for IBM Cloud Logs ingestion. Ensure you use the ingress endpoint. See https://cloud.ibm.com/docs/cloud-logs?topic=cloud-logs-endpoints_ingress."
   type        = string
+  default     = null
 }
 
 variable "cloud_logs_ingress_port" {
   type        = number
   default     = 3443
   description = "The target port for the IBM Cloud Logs ingestion endpoint. The port must be 443 if you connect by using a VPE gateway, or port 3443 when you connect by using CSEs."
-  validation {
-    error_message = "The Logs Routing supertenant ingestion port can only be `3443` or `443`."
-    condition     = contains([3443, 443], var.cloud_logs_ingress_port)
-  }
 }
