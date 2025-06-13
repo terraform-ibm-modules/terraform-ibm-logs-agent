6dfcf2529b1138accaf1c39d0d6bb3ce9e8e0563
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-06-13 03:30:33 +0000

    chore(deps): update terraform terraform-ibm-modules/cloud-logs/ibm to v1.5.0

--- examples/logs-agent-ocp/main.tf
+++ examples/logs-agent-ocp/main.tf
@@ -129,7 +129,7 @@ data "ibm_container_cluster_config" "cluster_config" {
 
 module "cloud_logs" {
   source            = "terraform-ibm-modules/cloud-logs/ibm"
-  version           = "1.4.0"
+  version           = "1.5.0"
   resource_group_id = module.resource_group.resource_group_id
   plan              = "standard"
   region            = var.region
