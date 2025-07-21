c79aa47b0a1f15b8ec41e4c78e1f292526df1394
kierra.searle@ibm.com <kierra.searle@ibm.com>
2025-07-21 10:01:26 -0500

    fix: pin to lowest version

--- examples/logs-agent-iks/version.tf
+++ examples/logs-agent-iks/version.tf
@@ -10,7 +10,7 @@ terraform {
     }
     helm = {
       source  = "hashicorp/helm"
-      version = "3.0.2"
+      version = "3.0.0"
     }
     # The kubernetes provider is not actually required by the module itself, just this example, so OK to use ">=" here instead of locking into a version
     kubernetes = {
