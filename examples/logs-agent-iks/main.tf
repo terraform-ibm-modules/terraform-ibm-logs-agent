7e3e5e7584a79b6895950eb3886611a069d5e156
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-07-22 04:09:16 +0100

    fix(deps): update terraform dependencies (#114)

--- examples/logs-agent-iks/main.tf
+++ examples/logs-agent-iks/main.tf
@@ -129,7 +129,7 @@ resource "time_sleep" "wait_operators" {
 
 module "cloud_logs" {
   source            = "terraform-ibm-modules/cloud-logs/ibm"
-  version           = "1.5.0"
+  version           = "1.5.11"
   resource_group_id = module.resource_group.resource_group_id
   plan              = "standard"
   region            = var.region
