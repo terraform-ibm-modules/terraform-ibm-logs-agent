ae96404e1f414c71d09623bfc80f7fcba8c942d0
Aashiq Jacob <122446118+Aashiq-J@users.noreply.github.com>
2025-08-04 22:22:13 +0530

    feat: improve DA user experience<br>- update catalog content<br>- modify IAM permissions listed for DA<br>- update reference architecture diagram<br>- added a new input variable `provider_visibility`<br>- update DA readme and DA docs (#118)
    * feat: add DA improvements
    
    * update diagram
    
    * review comments
    
    * update json
    
    * update
    
    * update icon
    
    * update digram
    
    * update

--- solutions/fully-configurable/variables.tf
+++ solutions/fully-configurable/variables.tf
@@ -230,3 +230,14 @@ variable "enable_multiline" {
   type        = bool
   default     = false
 }
+
+variable "provider_visibility" {
+  description = "Set the visibility value for the IBM terraform provider. Supported values are `public`, `private`, `public-and-private`. [Learn more](https://registry.terraform.io/providers/IBM-Cloud/ibm/latest/docs/guides/custom-service-endpoints)."
+  type        = string
+  default     = "private"
+
+  validation {
+    condition     = contains(["public", "private", "public-and-private"], var.provider_visibility)
+    error_message = "Invalid visibility option. Allowed values are 'public', 'private', or 'public-and-private'."
+  }
+}
