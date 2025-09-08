6a22ee6f90bbe5f84c94e03c464dbb7f0f89a117
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-09-08 11:34:48 +0000

    chore(deps): update dependencies

--- tests/resources/main.tf
+++ tests/resources/main.tf
@@ -74,7 +74,7 @@ locals {
 
 module "ocp_base" {
   source                              = "terraform-ibm-modules/base-ocp-vpc/ibm"
-  version                             = "3.56.0"
+  version                             = "3.56.1"
   resource_group_id                   = module.resource_group.resource_group_id
   region                              = var.region
   tags                                = var.resource_tags
@@ -93,7 +93,7 @@ module "ocp_base" {
 
 module "cos" {
   source                 = "terraform-ibm-modules/cos/ibm"
-  version                = "10.2.17"
+  version                = "10.2.18"
   resource_group_id      = module.resource_group.resource_group_id
   region                 = var.region
   cos_instance_name      = "${var.prefix}-cos"
@@ -116,7 +116,7 @@ locals {
 
 module "buckets" {
   source  = "terraform-ibm-modules/cos/ibm//modules/buckets"
-  version = "10.2.17"
+  version = "10.2.18"
   bucket_configs = [
     {
       bucket_name            = local.logs_bucket_name
@@ -140,7 +140,7 @@ module "buckets" {
 
 module "cloud_logs" {
   source            = "terraform-ibm-modules/cloud-logs/ibm"
-  version           = "1.6.22"
+  version           = "1.6.23"
   resource_group_id = module.ocp_base.resource_group_id
   region            = var.region
   instance_name     = "${var.prefix}-cloud-logs"
