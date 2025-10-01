d27a8fb2dfb8c3784321e9db9ff16627d7119d80
Jordan <jordanwilliams997@hotmail.com>
2025-10-01 13:07:58 +0100

    Change default prefix for resources to 'logs-agent-iks'

--- examples/logs-agent-iks/variables.tf
+++ examples/logs-agent-iks/variables.tf
@@ -7,7 +7,7 @@ variable "ibmcloud_api_key" {
 variable "prefix" {
   type        = string
   description = "A prefix for the name of all resources that are created by this example"
-  default     = "pub-iks"
+  default     = "logs-agent-iks"
 }
 
 variable "resource_group" {
