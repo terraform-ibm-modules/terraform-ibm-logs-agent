ef80ece735d80ec740bc506ae2867b2865686289
Jordan <jordanwilliams997@hotmail.com>
2025-10-07 09:31:34 +0100

    feat: updated the default helm repo to `oci://icr.io/ibm-observe` which does not require authentication (#182)

--- solutions/fully-configurable/provider.tf
+++ solutions/fully-configurable/provider.tf
@@ -15,12 +15,7 @@ provider "helm" {
     token                  = data.ibm_container_cluster_config.cluster_config.token
     cluster_ca_certificate = data.ibm_container_cluster_config.cluster_config.ca_certificate
   }
-  # IBM Cloud credentials are required to authenticate to the helm repo
-  registries = [{
-    url      = "oci://icr.io/ibm/observe/logs-agent-helm"
-    username = "iamapikey"
-    password = var.ibmcloud_api_key
-  }]
+  # No registry authentication required - using public registries
 }
 
 # Retrieve information about an existing VPC cluster
