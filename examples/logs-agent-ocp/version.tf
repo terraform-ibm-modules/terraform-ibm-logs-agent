94a7b73cd53d97d352dead5f6f1fb73c8459cd2a
Jordan <jordanwilliams997@hotmail.com>
2025-12-18 13:29:19 +0000

    Set IBM provider version to 1.86.0
    Updated IBM provider version to a specific version.

--- examples/logs-agent-ocp/version.tf
+++ examples/logs-agent-ocp/version.tf
@@ -7,7 +7,7 @@ terraform {
   required_providers {
     ibm = {
       source  = "ibm-cloud/ibm"
-      version = ">= 1.86.0, < 2.0.0"
+      version = "1.86.0"
     }
     helm = {
       source  = "hashicorp/helm"
