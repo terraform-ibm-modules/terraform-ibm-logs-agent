376885d87936be99cfe2b50ab7ab6cde8b1741e8
ocofaigh <ocofaigh@ie.ibm.com>
2026-01-05 12:58:58 +0000

    test: add workaround for known issue

--- tests/other_test.go
+++ tests/other_test.go
@@ -17,6 +17,9 @@ func TestRunAgentVpcOcp(t *testing.T) {
 	// Keep costs down by using the cloud_pak entitlement
 	options.TerraformVars["ocp_entitlement"] = "cloud_pak"
 
+	// Temp workaround for https://github.com/terraform-ibm-modules/terraform-ibm-base-ocp-vpc?tab=readme-ov-file#the-specified-api-key-could-not-be-found
+	createContainersApikey(t, options.Region, resourceGroup)
+
 	output, err := options.RunTestConsistency()
 	assert.Nil(t, err, "This should not have errored")
 	assert.NotNil(t, output, "Expected some output")
