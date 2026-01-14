495fa49a9cfe02b1037eefb921a6938ed169f5bf
Harika-Ponna <Harika.Ponna@ibm.com>
2026-01-14 13:38:23 +0530

    feat: add support for installing required dependencies

--- main.tf
+++ main.tf
@@ -28,14 +28,25 @@ locals {
   logs_agent_iam_api_key               = var.logs_agent_iam_api_key != null ? var.logs_agent_iam_api_key : ""
   logs_agent_trusted_profile_id        = var.logs_agent_trusted_profile_id != null ? var.logs_agent_trusted_profile_id : ""
   cloud_logs_ingress_endpoint          = var.cloud_logs_ingress_endpoint != null ? var.cloud_logs_ingress_endpoint : ""
-  logs_agent_additional_metadata = length(var.logs_agent_additional_metadata) > 0 ? merge([
+  logs_agent_additional_metadata       = length(var.logs_agent_additional_metadata) > 0 ? merge([
     for metadata in var.logs_agent_additional_metadata : {
       (metadata.key) = metadata.value
   }]...) : {}                                                                                                                                       # DO NOT REMOVE "...", it is used to convert list of objects into a single object
   cluster_name = var.is_vpc_cluster ? data.ibm_container_vpc_cluster.cluster[0].resource_name : data.ibm_container_cluster.cluster[0].resource_name # Not publicly documented in provider. See https://github.com/IBM-Cloud/terraform-provider-ibm/issues/4485
+  binaries_path                        = "/tmp"
+}
+
+resource "null_resource" "install_required_binaries" {
+  count = var.install_required_binaries ? 1 : 0
+
+  provisioner "local-exec" {
+    command     = "${path.module}/scripts/install-binaries.sh ${local.binaries_path}"
+    interpreter = ["/bin/bash", "-c"]
+  }
 }
 
 resource "helm_release" "logs_agent" {
+  depends_on = [null_resource.install_required_binaries]
   name             = var.logs_agent_name
   chart            = var.logs_agent_chart
   repository       = var.logs_agent_chart_location
@@ -159,9 +170,8 @@ resource "helm_release" "logs_agent" {
     })
   ]
 
-
   provisioner "local-exec" {
-    command     = "${path.module}/scripts/confirm-rollout-status.sh ${var.logs_agent_name} ${var.logs_agent_namespace}"
+    command     = "${path.module}/scripts/confirm-rollout-status.sh ${var.logs_agent_name} ${var.logs_agent_namespace} ${local.binaries_path}"
     interpreter = ["/bin/bash", "-c"]
     environment = {
       KUBECONFIG = data.ibm_container_cluster_config.cluster_config.config_file_path
