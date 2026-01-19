cace4cf74692fd93998b04fe2d202148082353e8
Harika-Ponna <Harika.Ponna@ibm.com>
2026-01-19 12:38:40 +0530

    fix : removed bin_path variable

--- scripts/confirm-rollout-status.sh
+++ scripts/confirm-rollout-status.sh
@@ -3,9 +3,8 @@ set -e
 
 daemonset=$1
 namespace=$2
-BIN_PATH=${3:-"/tmp"}
 
 # The binaries downloaded by the install-binaries script are located in the /tmp directory.
-export PATH=$PATH:${BIN_PATH}
+export PATH=$PATH:${3:-"/tmp"}
 
 kubectl rollout status ds "${daemonset}" -n "${namespace}" --timeout 30m
