29527ba028c6b7aec701c248f83098398b4e360b
Harika-Ponna <Harika.Ponna@ibm.com>
2026-02-02 15:48:30 +0530

    fix: update script to only install kubectl

--- variables.tf
+++ variables.tf
@@ -309,6 +309,6 @@ variable "storage_name" {
 variable "install_required_binaries" {
   type        = bool
   default     = true
-  description = "When set to true, a script will run to check if `kubectl` and `jq` exist on the runtime and if not attempt to download them from the public internet and install them to /tmp. Set to false to skip running this script."
+  description = "When set to true, a script will run to check if `kubectl` exist on the runtime and if not attempt to download it from the public internet and install it to /tmp. Set to false to skip running this script."
   nullable    = false
 }
