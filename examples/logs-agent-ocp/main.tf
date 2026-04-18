0c90566baa86649ff4289b73d9397512dec3a2ba
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2026-04-18 04:20:21 +0100

    fix(deps): update dependencies (#296)

--- examples/logs-agent-ocp/main.tf
+++ examples/logs-agent-ocp/main.tf
@@ -131,7 +131,7 @@ data "ibm_container_cluster_config" "cluster_config" {
 
 module "cloud_logs" {
   source            = "terraform-ibm-modules/cloud-logs/ibm"
-  version           = "1.13.0"
+  version           = "1.13.1"
   resource_group_id = module.resource_group.resource_group_id
   plan              = "standard"
   region            = var.region
