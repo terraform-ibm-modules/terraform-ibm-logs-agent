ac3b6b178bad010fa1de8786bf26ee892f693f17
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-07-08 11:20:39 +0000

    chore(deps): update terraform terraform-ibm-modules/cos/ibm to v10.17.3

--- tests/resources/main.tf
+++ tests/resources/main.tf
@@ -93,7 +93,7 @@ module "ocp_base" {
 
 module "cos" {
   source                 = "terraform-ibm-modules/cos/ibm"
-  version                = "10.17.2"
+  version                = "10.17.3"
   resource_group_id      = module.resource_group.resource_group_id
   region                 = var.region
   cos_instance_name      = "${var.prefix}-cos"
@@ -115,7 +115,7 @@ locals {
 
 module "buckets" {
   source  = "terraform-ibm-modules/cos/ibm//modules/buckets"
-  version = "10.17.2"
+  version = "10.17.3"
   bucket_configs = [
     {
       bucket_name            = local.logs_bucket_name
